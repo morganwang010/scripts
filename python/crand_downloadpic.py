f54191439b574faa31143601c678015e864edb92
morganwang010 <morganwang010@sina.com>
2020-11-24 06:25:41 +0800

    20201124

---
+++ python/crand_downloadpic.py
@@ -0,0 +1,61 @@
+ 
+import uuid
+import json
+import urllib.request
+import string
+import sys,time  
+import os
+import socket  
+import random
+import threading
+import os,re,sys
+targetDir = r"/data/crane"  
+def destFile(path):  
+    if not os.path.isdir(targetDir):  
+        os.mkdir(targetDir)  
+    pos = path.rindex('/')
+    a = random.randint(1,10000)
+    ss = uuid.uuid1()
+    b = '%d' %a  
+    ss = uuid.uuid1()
+    ss = str(ss)[:8]
+    t = os.path.join(targetDir,  b+ss+path[pos+1:]) 
+    return t  
+
+def getPic(link):
+    try:
+        urllib.request.urlretrieve(link, destFile(link))
+    except:
+        pass     
+		
+def urlOpen(url):
+	try:
+        u = urllib.request.urlopen(url)
+    except:
+        pass
+	d = json.loads(u.read().decode())
+    t = re.findall(r'([^\s]*(\/))',d['thumbnail_url'])
+	link = t[0][0] + '2048.jpg'
+	try:
+        urllib.request.urlretrieve(link, destFile(link))
+    except:
+        pass 
+		
+if __name__ == "__main__":  
+    m = 0
+    threads = []
+    threadsOpen = []
+    for i in range(1,114):
+        #url='http://500px.com/photo/%d/oembed.json' %i
+        url='https://photo.lmjx.net/p_6_13_0_%d.html' %i
+        socket.setdefaulttimeout(5)
+        if(m<10):
+            th = threading.Thread(target = urlOpen,args=(url,))
+            threads.append(th)
+            m=m+1
+            continue
+        else:
+            for s in range(m):
+                threads[s].start()
+            m = 0
+            threads = []
